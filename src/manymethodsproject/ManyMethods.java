66cffa76d5ec972d1d9ef17f68bafb2ce3bc04b7
MON15150511 <MON15150511@R206-STUDPC-115.runshaw.net>
2015-11-17 12:41:01 +0000

    Added root finder.

--- src/manymethodsproject/ManyMethods.java
+++ src/manymethodsproject/ManyMethods.java
@@ -2,6 +2,15 @@ package manymethodsproject;
 
 public class ManyMethods
 {
+
+    public ManyMethods()
+    {
+        System.out.println("******************");
+        System.out.println("CREATING AN OBJECT");
+        System.out.println("******************");
+    }
+    
+    
     public void myProcedure()
     {
         System.out.println("Non returining procedure");
@@ -16,11 +25,23 @@ public class ManyMethods
         System.out.println("Parameter passed in is "+ theParam);
     }
     
+    public double IterativeSolution(int root, int iterations)
+    {
+        double solution=root/2;
+        for(int i=0;i<iterations+1;i++)
+        {
+            //solution=(1/2) * (solution+(2/solution));
+            solution = (solution+(root/solution))/2;
+        }
+        return solution;
+    }
+    
     public void runProgram()
     {
         myProcedure();
         System.out.println(myFunction());
         myParameter(27);
+        System.out.println(IterativeSolution(25, 100));
     }
     
     public static void main(String[] args)
